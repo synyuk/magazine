ea879891e98f9c310fa028ae0b82b8f1ce0786f2
Andrey Synyuk <andrey.sinyuk@triogeeks.com>
2024-12-25 18:52:22 +0200

    first commit

---
+++ js/data.js
@@ -0,0 +1,111 @@
+export let product = [
+    {
+        id: 1,
+        sale: "",
+        pic: "img/products/product-4.png",
+        company: "Sony",
+        name: "4K TV Game Mini Arcade Rocker Console 32GB/64GB",
+        rate: 4,
+        price: "$29.80"
+    },
+    {
+        id: 2,
+        sale: "-15%",
+        pic: "img/products/product-5.png",
+        company: "Google",
+        name: "Cancelling Headset Music Sport Deep Bass",
+        rate: 2,
+        price: "$39.80"
+    },
+    {
+        id: 3,
+        sale: "-10%",
+        pic: "img/products/product-6.png",
+        company: "Apple",
+        name: "T500BT Original Wireless Bluetooth Headphone",
+        rate: 5,
+        price: "$69.80"
+    },
+    {
+        id: 4,
+        sale: "-10%",
+        pic: "img/products/product-7.png",
+        company: "Toshiba",
+        name: "Black Walnut Wood & Aluminum Headphone Stand",
+        rate: 4,
+        price: "$70.80"
+    },
+    {
+        id: 5,
+        sale: "",
+        pic: "img/products/product-8.png",
+        company: "BassX",
+        name: "Beats Studio3 Wireless Bluetooth Headphones",
+        rate: 5,
+        price: "$29.80"
+    },
+    {
+        id: 6,
+        sale: "",
+        pic: "img/products/product-9.png",
+        company: "Konika",
+        name: "Marshall MAJOR III Wireless Folding Bluetooth",
+        rate: 5,
+        price: "$39.80"
+    },
+    {
+        id: 7,
+        sale: "",
+        pic: "img/products/product-10.png",
+        company: "Apple",
+        name: "T500BT Original Wireless Bluetooth Headphone",
+        rate: 5,
+        price: "$69.80"
+    },
+    {
+        id: 8,
+        sale: "",
+        pic: "img/products/product-11.png",
+        company: "Toshiba",
+        name: "Black Walnut Wood & Aluminum Headphone Stand",
+        rate: 5,
+        price: "$70.80"
+    },
+    {
+        id: 9,
+        sale: "",
+        pic: "img/products/product-12.png",
+        company: "BassX",
+        name: "Beats Studio3 Wireless Bluetooth Headphones",
+        rate: 5,
+        price: "$29.80"
+    },
+    {
+        id: 10,
+        sale: "",
+        pic: "img/products/product-13.png",
+        company: "Konika",
+        name: "Marshall MAJOR III Wireless Folding Bluetooth",
+        rate: 5,
+        price: "$39.80"
+    },
+    {
+        id: 11,
+        sale: "",
+        pic: "img/products/product-14.png",
+        company: "Apple",
+        name: "T500BT Original Wireless Bluetooth Headphone",
+        rate: 5,
+        price: "$69.80"
+    }
+    ,
+    {
+        id: 12,
+        sale: "",
+        pic: "img/products/product-15.png",
+        company: "Toshiba",
+        name: "Black Walnut Wood & Aluminum Headphone Stand",
+        rate: 5,
+        price: "$70.80"
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,58 @@
+import {product} from "./data.js";
+
+function createProduct(sale, pic, company, name, rate, price) {
+    let productWrapper = document.querySelector(".product-grid");
+    let productItem = document.createElement("div");
+    productItem.classList.add("product");
+    if(sale) {
+        productItem.innerHTML += `<div class="product-sale">${sale}</div>`
+    }
+    if(pic) {
+        productItem.innerHTML += `<div class="product-image">
+            <img src="${pic}" alt="">
+        </div>`
+    }
+    if(company) {
+        productItem.innerHTML += `<div class="product-company">${company}</div>`
+    }
+    if(name) {
+        productItem.innerHTML += `<div class="product-name">${name}</div>`
+    }
+    if(rate) {
+        let rateIcon = '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.99996 0L4.77994 4.37495L0 5.16906L3.40783 8.66706L2.6738 13.5328L7 11.3197L11.3262 13.5328L10.5922 8.66706L14 5.16906L9.22006 4.37495L6.99996 0Z" fill="#FFD166"/></svg>'
+        let rateIconMimus = '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.99996 0L4.77994 4.37495L0 5.16906L3.40783 8.66706L2.6738 13.5328L7 11.3197L11.3262 13.5328L10.5922 8.66706L14 5.16906L9.22006 4.37495L6.99996 0Z" fill="#DFDEDC"/></svg>'
+        let rateWrapper = document.createElement("div");
+        rateWrapper.classList.add("product-rate");
+
+        for(let i = 1; i <= rate; i++) {
+            rateWrapper.innerHTML += `${rateIcon}`;
+        }
+
+        if(rate<5){
+            let minus = 5 - rate;
+            for(let i = 1; i <= minus; i++) {
+                rateWrapper.innerHTML += `${rateIconMimus}`;
+            }
+        }
+        productItem.append(rateWrapper);
+
+        let bottomWrapper = document.createElement("div");
+        bottomWrapper.classList.add("product-bottom");
+        productItem.append(bottomWrapper);
+
+        if(price) {
+            bottomWrapper.innerHTML += `<div class="product-price">${price}</div>`
+        }
+
+        let buttonAdd = document.createElement("div");
+        buttonAdd.classList.add("product-add");
+        buttonAdd.textContent = "Add  +";
+        bottomWrapper.append(buttonAdd);
+
+    }
+    productWrapper.append(productItem);
+
+}
+    for (let item of product) {
+        createProduct(item.sale, item.pic, item.company, item.name, item.rate, item.price);
+    }
