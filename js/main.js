eead3846481f10a73d2c17b55cde504549e492f1
Andrey Synyuk <andrey.sinyuk@triogeeks.com>
2024-12-27 17:34:19 +0200

    add map

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import {product} from "./data.js";
 
+for (let item of product) {
+    createProduct(item.sale, item.pic, item.company, item.name, item.rate, item.price);
+}
+
 function createProduct(sale, pic, company, name, rate, price) {
     let productWrapper = document.querySelector(".product-grid");
     let productItem = document.createElement("div");
@@ -53,6 +57,16 @@ function createProduct(sale, pic, company, name, rate, price) {
     productWrapper.append(productItem);
 
 }
-    for (let item of product) {
-        createProduct(item.sale, item.pic, item.company, item.name, item.rate, item.price);
-    }
+
+
+//MAP
+var map = L.map('map').setView([50.44771846014896, 30.492616380116534], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([50.44771846014896, 30.492616380116534]).addTo(map);
+
+marker.bindPopup("<b>Our Office</b>").openPopup();
